3e775fb554f2f57a567b5c6440990bf797c1bfeb
MOVIDEV <itdev@movi.id>
2026-09-04 15:57:01 +0700

    nyambungin ke db pslid_itasset_db

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,9 @@ app.use(express.json());
 
 const db = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: 'root', // Sesuaikan password MAMP kamu
-  database: 'it_asset_db',
+  user: 'pslid_auldatabase',
+  password: '#punyaAul', // Sesuaikan password MAMP kamu
+  database: 'pslid_it_asset_db',
   port: 8889,       // Sesuaikan port MAMP kamu
   waitForConnections: true,
   connectionLimit: 10,
